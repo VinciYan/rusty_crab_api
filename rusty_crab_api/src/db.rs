dc11971e4532eff1c446133c992f7933ecb75b15
VinciYan <36335240+VinciYan@users.noreply.github.com>
2024-09-13 21:54:26 +0800

    更新 db.rs

--- rusty_crab_api/src/db.rs
+++ rusty_crab_api/src/db.rs
@@ -1,7 +1,7 @@
 use sea_orm::DatabaseConnection;
 
 pub async fn establish_connection() -> DatabaseConnection {
-    sea_orm::Database::connect("postgres://postgres:961010NBMM@vinciyan.com:15003/mydb")
+    sea_orm::Database::connect("postgres://postgres:123456@xxx.com:1234/mydb")
         .await
         .expect("Failed to connect to database")
 }
